5c17a903b31495e25353b5ae9fcf53c80b27f15a
Derek Hayes <derek2099@gmail.com>
2024-07-20 15:39:19 -0500

    Create fragment to display time slots for reservations

---
+++ app/src/main/java/dev/derekhayes/reservations/SelectTimeFragment.java
@@ -0,0 +1,17 @@
+package dev.derekhayes.reservations;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SelectTimeFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_select_time, container, false);
+    }
+}
